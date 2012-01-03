ef3cb65efc5e67ad7e5eff4da0098fa0d78f7a36
Albin Kerouanton <stratege3@gmail.com>
2012-01-03 17:46:43 +0100

    Correction de quelques bugs.

--- src/DP/Core/MachineBundle/PHPSeclibWrapper/Exception.php
+++ src/DP/Core/MachineBundle/PHPSeclibWrapper/Exception.php
@@ -2,28 +2,36 @@
 namespace PHPSeclibWrapper\Exception;
 use DP\Core\MachineBundle\PHPSeclibWrapper\PHPSeclibWrapper;
 
-class FileNotFoundException extends \Exception {
-    public function __construct($message, PHPSeclibWrapper $srv) {
+class FileNotFoundException extends \Exception
+{
+    public function __construct($message, PHPSeclibWrapper $srv)
+    {
         parent::__construct('The private keyfile for ' . $srv->getUser() . '@' .
             $srv->getHost() . ' ' . $message);
     }  
 }
-class EmptyKeyfileException extends \Exception {
-    public function __construct(PHPSeclibWrapper $srv) {
+class EmptyKeyfileException extends \Exception
+{
+    public function __construct(PHPSeclibWrapper $srv)
+    {
         parent::__construct('The private keyfile for ' . $srv->getUser() . '@' .
              $srv->getHost() . ':' . $srv->getPort() . 'is empty.');
     }
 }
 
-class ConnectionErrorException extends \Exception {
-    public function __construct(PHPSeclibWrapper $srv) {
+class ConnectionErrorException extends \Exception
+{
+    public function __construct(PHPSeclibWrapper $srv)
+    {
         parent::__construct('Connection to ' . $srv->getUser() . '@' . 
             $srv->getHost() .':' . $srv->getPort() . ' failed.');
     }
 }
 
-class IncompleteLoginIDException extends \Exception {
-    public function __construct(PHPSeclibWrapper $srv) {
+class IncompleteLoginIDException extends \Exception
+{
+    public function __construct(PHPSeclibWrapper $srv)
+    {
         parent::__construct('Incomplete login IDs for ' . $srv->getUser() . 
             '@' . $srv->getHost() . ':' . $srv->getPort());
     }
--- src/DP/Core/UserBundle/Entity/User.php
+++ src/DP/Core/UserBundle/Entity/User.php
@@ -31,4 +31,9 @@ class User extends BaseUser {
     {
         return $this->id;
     }
+	
+    public function __construct()
+    {
+        parent::__construct();
+    }
 }
