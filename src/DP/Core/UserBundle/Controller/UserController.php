d9168b99c8d66428cdf9c36c49b0f3c8a5218d6a
Albin Kerouanton <stratege3@gmail.com>
2012-01-26 15:57:44 +0100

    Integration du design a la zone d'identification, ajout de menus intermediaires

--- src/DP/Core/UserBundle/Controller/UserController.php
+++ src/DP/Core/UserBundle/Controller/UserController.php
@@ -1,11 +1,43 @@
 <?php
 
+/*
+** Copyright (C) 2010-2012 Kerouanton Albin, Smedts Jérôme
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License along
+** with this program; if not, write to the Free Software Foundation, Inc.,
+** 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 namespace DP\Core\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * @author Albin Kerouanton 
+ */
 class UserController extends Controller {
-	public function menuAction() {
-		return $this->render('DPUserBundle:User:menu.html.twig');
+	public function homeMenuAction()
+    {
+		return $this->render('DPUserBundle:User:home.html.twig');
 	}
+    
+    public function gameMenuAction()
+    {
+        return $this->render('DPUserBundle:User:gameMenu.html.twig');
+    }
+    
+    public function configMenuAction()
+    {
+        return $this->render('DPUserBundle:User:configMenu.html.twig');
+    }
 }
 ?>
