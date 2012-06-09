f443bc6b960c15a22b103c74ceac668c5f4f711d
Albin Kerouanton <stratege3@gmail.com>
2012-06-09 12:46:24 +0200

    Ajout des DoctrineMigrationsBundle afin de gerer la bdd entre les release.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,6 +31,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\DoctrineMigrationsBundle\DoctrineMigrationsBundle(),
             new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
--- app/autoload.php
+++ app/autoload.php
@@ -27,13 +27,17 @@ $loader->registerNamespaces(array(
     'Sensio'           => __DIR__.'/../vendor/bundles',
     'JMS'              => __DIR__.'/../vendor/bundles',
     'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
+    'Doctrine\\DBAL\\Migrations' => __DIR__.'/../vendor/doctrine-migrations/lib',
     'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
     'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
     'Monolog'          => __DIR__.'/../vendor/monolog/src',
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
 	'FOS'			   => __DIR__.'/../vendor/bundles', 
-    'Sonata'           => __DIR__.'/../vendor/bundles',     
+    'Sonata'           => array(
+        __DIR__.'/../vendor/bundles',
+        __DIR__.'/../vendor/sonata-doctrine-extensions/src',
+    ), 
     'Knp\\Bundle'      => __DIR__.'/../vendor/bundles',
     'Knp\\Menu'        => __DIR__.'/../vendor/knp/menu/src',
 ));
