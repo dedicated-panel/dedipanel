07e9c870c2df88907c34ed175a62a5dd80105666
Albin Kerouanton <stratege3@gmail.com>
2013-03-22 00:07:32 +0100

    Maj de l'UpdateWatcher

--- src/Application/Sonata/UserBundle/Service/UpdateWatcherService.php
+++ src/Application/Sonata/UserBundle/Service/UpdateWatcherService.php
@@ -38,14 +38,18 @@ class UpdateWatcherService
             )
         ));
         
-        $version = json_decode(@file_get_contents('http://www.dedicated-panel.net/version.json', false, $context));
-        $this->versionAvailable = $version->version;
-                    
-        if (version_compare($this->versionAvailable, $this->currentVersion) == 1) {
-            $this->updateAvailable = true;
-        }
-        else {
-            $this->updateAvailable = false;
+        $version = @file_get_contents('http://www.dedicated-panel.net/version.json', false, $context);
+        
+        if (strlen($version) > 0) {
+            $version = json_decode($version);
+            $this->versionAvailable = $version->version;
+            
+            if (version_compare($this->versionAvailable, $this->currentVersion) == 1) {
+                $this->updateAvailable = true;
+            }
+            else {
+                $this->updateAvailable = false;
+            }
         }
         
         return $this->updateAvailable;
