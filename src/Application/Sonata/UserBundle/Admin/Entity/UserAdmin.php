62135e4c1768b8d479b6a339b406561a9bfc73c1
Albin Kerouanton <stratege3@gmail.com>
2013-03-23 21:00:08 +0100

    Correction des pages d'administration de la partie utilisateur.

---
+++ src/Application/Sonata/UserBundle/Admin/Entity/UserAdmin.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Application\Sonata\UserBundle\Admin\Entity;
+
+use Sonata\AdminBundle\Admin\Admin;
+
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use FOS\UserBundle\Model\UserManagerInterface;
+
+class UserAdmin extends Admin
+{
+    protected $formOptions = array(
+        'validation_groups' => 'Profile'
+    );
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('username')
+            ->add('email')
+            ->add('groups')
+            ->add('enabled', null, array('editable' => true))
+            ->add('locked', null, array('editable' => true))
+            ->add('createdAt')
+        ;
+
+        if ($this->isGranted('ROLE_ALLOWED_TO_SWITCH')) {
+            $listMapper
+                ->add('impersonating', 'string', array('template' => 'SonataUserBundle:Admin:Field/impersonating.html.twig'))
+            ;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureDatagridFilters(DatagridMapper $filterMapper)
+    {
+        $filterMapper
+            ->add('id')
+            ->add('username')
+            ->add('locked')
+            ->add('email')
+            ->add('groups')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->with('General')
+                ->add('username')
+                ->add('email')
+            ->end()
+            ->with('Groups')
+                ->add('groups')
+            ->end()
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General')
+                ->add('username')
+                ->add('email')
+                ->add('plainPassword', 'text', array('required' => false))
+            ->end()
+            ->with('Groups')
+                ->add('groups', 'sonata_type_model', array('required' => false, 'expanded' => true, 'multiple' => true))
+            ->end()
+        ;
+
+        if (!$this->getSubject()->hasRole('ROLE_SUPER_ADMIN')) {
+            $formMapper
+                ->with('Management')
+                    ->add('roles', 'sonata_security_roles', array(
+                        'expanded' => true,
+                        'multiple' => true,
+                        'required' => false
+                    ))
+                    ->add('locked', null, array('required' => false))
+                    ->add('expired', null, array('required' => false))
+                    ->add('enabled', null, array('required' => false))
+                    ->add('credentialsExpired', null, array('required' => false))
+                ->end()
+            ;
+        }
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function preUpdate($user)
+    {
+        $this->getUserManager()->updateCanonicalFields($user);
+        $this->getUserManager()->updatePassword($user);
+    }
+
+    /**
+     * @param UserManagerInterface $userManager
+     */
+    public function setUserManager(UserManagerInterface $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+    /**
+     * @return UserManagerInterface
+     */
+    public function getUserManager()
+    {
+        return $this->userManager;
+    }
+}
