0f02a15b0f69322c7f70573ebb74fc62740f0210
Albin Kerouanton <stratege3@gmail.com>
2013-03-29 21:35:05 +0100

    Modification de la date dans le block de licence en en-tete de chaque fichier (+ ajout si non present).

--- src/DP/Core/MachineBundle/Tests/PHPSeclibWrapper/PHPSeclibWrapperTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace DP\Core\MachineBundle\Tests\PHPSeclibWrapper;
-use DP\Core\MachineBundle\PHPSeclibWrapper\PHPSeclibWrapper;
-
-// TODO: Unit Tests
-class PHPSeclibWrapperTest extends \PHPUnit_Framework_TestCase {
-}
-?>
