7fef5dddaa7a11ef34502ecd9071933fb54b60bc
NiR- <stratege3@gmail.com>
2014-01-05 23:44:26 +0100

    Uniformisation des traductions.

--- src/DP/Core/MachineBundle/Form/BaseMachineType.php
+++ src/DP/Core/MachineBundle/Form/BaseMachineType.php
@@ -28,10 +28,10 @@ abstract class BaseMachineType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('privateIp', 'text', array('label' => 'machine.privateIp'))
-            ->add('publicIp', 'text', array('label' => 'machine.publicIp', 'required' => false))
-            ->add('port', 'number', array('label' => 'machine.port'))
-            ->add('user', 'text', array('label' => 'machine.user'))
+            ->add('privateIp', 'text', array('label' => 'machine.fields.private_ip'))
+            ->add('publicIp', 'text', array('label' => 'machine.fields.public_ip', 'required' => false))
+            ->add('port', 'number', array('label' => 'machine.fields.port'))
+            ->add('user', 'text', array('label' => 'machine.fields.user'))
         ;
     }
 }
