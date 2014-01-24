2e4554953afb23e181a4e4a875401c145edb08e2
NiR- <stratege3@gmail.com>
2014-01-24 18:15:04 +0100

    Suppression des passes de compilation inutiles.

--- src/DP/Core/UserBundle/DPUserBundle.php
+++ src/DP/Core/UserBundle/DPUserBundle.php
@@ -3,20 +3,9 @@
 namespace DP\Core\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use DP\Core\UserBundle\DependencyInjection\Compiler\AddChildrenRoleBuilderCompilerPass;
-use DP\Core\UserBundle\DependencyInjection\Compiler\AddDedipanelRolesCompilerPass;
-use DP\Core\UserBundle\DependencyInjection\Compiler\RemoveTopRoleBuilderCompilerPass;
 
 class DPUserBundle extends Bundle
 {
-    public function build(ContainerBuilder $builder)
-    {
-        $builder->addCompilerPass(new AddChildrenRoleBuilderCompilerPass);
-        $builder->addCompilerPass(new AddDedipanelRolesCompilerPass);
-        $builder->addCompilerPass(new RemoveTopRoleBuilderCompilerPass);
-    }
-    
     public function getParent()
     {
         return 'FOSUserBundle';
