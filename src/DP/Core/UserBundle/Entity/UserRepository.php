31e436a82ab5313944d53c95405960dc4606fb82
NiR- <stratege3@gmail.com>
2014-02-23 22:51:59 +0100

    Ajout d'un filtrage selon les groupes accessibles par l'utilisateur courant sur l'index des groupes et des utilisateurs.

--- src/DP/Core/UserBundle/Entity/UserRepository.php
+++ src/DP/Core/UserBundle/Entity/UserRepository.php
@@ -3,6 +3,7 @@
 namespace DP\Core\UserBundle\Entity;
 
 use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 
 class UserRepository extends EntityRepository
 {
@@ -15,5 +16,18 @@ class UserRepository extends EntityRepository
         $new->setCreatedAt(new \DateTime);
         
         return $new;
+    }
+
+    protected function applyCriteria(QueryBuilder $queryBuilder, array $criteria = null)
+    {
+        if (isset($criteria['groups'])) {
+            $queryBuilder
+                ->innerJoin($this->getAlias() . '.groups', 'g', 'WITH', $queryBuilder->expr()->in('g.id', $criteria['groups']))
+            ;
+            
+            unset($criteria['groups']);
+        }
+        
+        parent::applyCriteria($queryBuilder, $criteria);
     }
 }
