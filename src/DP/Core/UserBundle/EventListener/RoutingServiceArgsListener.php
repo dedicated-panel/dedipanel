deb92a6d5fc4acd90af2dff0a505c36a1c6622e4
NiR- <stratege3@gmail.com>
2014-02-23 22:34:58 +0100

    Ajout d'un listener convertissant les parametres referecant des services.

---
+++ src/DP/Core/UserBundle/EventListener/RoutingServiceArgsListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DP\Core\UserBundle\EventListener;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class RoutingServiceArgsListener extends ContainerAware
+{
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        $params  = $request->attributes->get('_route_params');
+        
+        if (isset($params['_sylius']) && isset($params['_sylius']['criteria'])) {
+            $params['_sylius']['criteria'] = $this->convertServiceArgs($params['_sylius']['criteria']);
+            
+            $request->attributes->set('_sylius', $params['_sylius']);
+            $request->attributes->set('_route_params', $params);
+        }
+    }
+    
+    public function convertServiceArgs($criteria = array())
+    {
+        foreach ($criteria AS &$crit) {
+            if (preg_match('#^\@(.+)\:(.+)$#', $crit, $matches)) {
+                $service = $this->container->get($matches[1]);
+                $value = $service->{$matches[2]}();
+                
+                $crit = $value;
+            }
+        }
+        
+        return $criteria;
+    }
+}
