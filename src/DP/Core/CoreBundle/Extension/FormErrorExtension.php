690997486896b23deea5b6eafea47968caceb03f
NiR- <stratege3@gmail.com>
2014-07-04 21:54:34 +0200

    Ajout form extension pour afficher les erreurs en haut des formulaires.

---
+++ src/DP/Core/CoreBundle/Extension/FormErrorExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DP\Core\CoreBundle\Extension;
+
+use Symfony\COmponent\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+
+class FormErrorExtension extends AbstractTypeExtension
+{
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['errors'] = $form->getErrors(true, true);
+    }
+
+    public function getExtendedType()
+    {
+        return 'form';
+    }
+}
