1af9aa6f9735aa857ce94d9a76581b5984f898ca
NiR- <stratege3@gmail.com>
2014-07-31 19:07:49 +0200

    Correction de la déclaration de la ressource "groupe" pour que le repository de l'entité soit utilisé.

--- src/DP/Core/UserBundle/Entity/GroupRepository.php
+++ src/DP/Core/UserBundle/Entity/GroupRepository.php
@@ -4,9 +4,197 @@ namespace DP\Core\UserBundle\Entity;
 
 use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 use DP\Core\UserBundle\Entity\Group;
+use Doctrine\ORM\QueryBuilder;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
 
-class GroupRepository extends NestedTreeRepository
+class GroupRepository extends NestedTreeRepository implements RepositoryInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function createNew()
+    {
+        $className = $this->getClassName();
+
+        return new $className;
+    }
+
+    /**
+     * @param mixed $id
+     *
+     * @return null|object
+     */
+    public function find($id)
+    {
+        return $this
+            ->getQueryBuilder()
+            ->andWhere($this->getAlias().'.id = '.intval($id))
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
+    /**
+     * @return array
+     */
+    public function findAll()
+    {
+        return $this
+            ->getCollectionQueryBuilder()
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * @param array $criteria
+     *
+     * @return null|object
+     */
+    public function findOneBy(array $criteria)
+    {
+        $queryBuilder = $this->getQueryBuilder();
+
+        $this->applyCriteria($queryBuilder, $criteria);
+
+        return $queryBuilder
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
+    /**
+     * @param array   $criteria
+     * @param array   $orderBy
+     * @param integer $limit
+     * @param integer $offset
+     *
+     * @return array
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        $queryBuilder = $this->getCollectionQueryBuilder();
+
+        $this->applyCriteria($queryBuilder, $criteria);
+        $this->applySorting($queryBuilder, $orderBy);
+
+        if (null !== $limit) {
+            $queryBuilder->setMaxResults($limit);
+        }
+
+        if (null !== $offset) {
+            $queryBuilder->setFirstResult($offset);
+        }
+
+        return $queryBuilder
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createPaginator(array $criteria = null, array $orderBy = null)
+    {
+        $queryBuilder = $this->getCollectionQueryBuilder();
+
+        $this->applyCriteria($queryBuilder, $criteria);
+        $this->applySorting($queryBuilder, $orderBy);
+
+        return $this->getPaginator($queryBuilder);
+    }
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     *
+     * @return Pagerfanta
+     */
+    public function getPaginator(QueryBuilder $queryBuilder)
+    {
+        return new Pagerfanta(new DoctrineORMAdapter($queryBuilder));
+    }
+
+    /**
+     * @return QueryBuilder
+     */
+    protected function getQueryBuilder()
+    {
+        return $this->createQueryBuilder($this->getAlias());
+    }
+
+    /**
+     * @return QueryBuilder
+     */
+    protected function getCollectionQueryBuilder()
+    {
+        return $this->createQueryBuilder($this->getAlias());
+    }
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     *
+     * @param array $criteria
+     */
+    protected function applyCriteria(QueryBuilder $queryBuilder, array $criteria = null)
+    {
+        if (null === $criteria) {
+            return;
+        }
+
+        foreach ($criteria as $property => $value) {
+            if (null === $value) {
+                $queryBuilder
+                    ->andWhere($queryBuilder->expr()->isNull($this->getPropertyName($property)));
+            } elseif (is_array($value)) {
+                $queryBuilder->andWhere($queryBuilder->expr()->in($this->getPropertyName($property), $value));
+            } elseif ('' !== $value) {
+                $queryBuilder
+                    ->andWhere($queryBuilder->expr()->eq($this->getPropertyName($property), ':' . $property))
+                    ->setParameter($property, $value);
+            }
+        }
+    }
+
+    /**
+     * @param QueryBuilder $queryBuilder
+     *
+     * @param array $sorting
+     */
+    protected function applySorting(QueryBuilder $queryBuilder, array $sorting = null)
+    {
+        if (null === $sorting) {
+            return;
+        }
+
+        foreach ($sorting as $property => $order) {
+            if (!empty($order)) {
+                $queryBuilder->orderBy($this->getPropertyName($property), $order);
+            }
+        }
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function getPropertyName($name)
+    {
+        if (false === strpos($name, '.')) {
+            return $this->getAlias().'.'.$name;
+        }
+
+        return $name;
+    }
+
+    protected function getAlias()
+    {
+        return 'o';
+    }
+
     public function getQBFindIsNot(Group $group)
     {
         $qb = $this->getQueryBuilder();
