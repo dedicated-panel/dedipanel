a820e3756bd501261a705f431ab998efaa832545
NiR- <stratege3@gmail.com>
2014-08-29 17:07:00 +0200

    Correction js add minecraft.

--- src/DP/GameServer/MinecraftServerBundle/Resources/public/js/add.js
+++ src/DP/GameServer/MinecraftServerBundle/Resources/public/js/add.js
@@ -20,15 +20,15 @@ $(function() {
     // Change la valeur du champ queryPort selon le champ port uniquement si queryPort est vide
     var userModified = false;
     
-    $('#dp_gameserver_minecraftserverbundle_addminecraftservertype_port').bind('blur', function() {
+    $('#dedipanel_minecraft_port').bind('blur', function() {
         if (!userModified) {
-            $('#dp_gameserver_minecraftserverbundle_addminecraftservertype_queryPort').val(
+            $('#dedipanel_minecraft_queryPort').val(
                 $(this).val()
             );
         }
     });
     
-    $('#dp_gameserver_minecraftserverbundle_addminecraftservertype_queryPort').bind('blur', function() {
+    $('#dedipanel_minecraft_queryPort').bind('blur', function() {
         if ($(this).val() != '') {
             userModified = true;
         }
