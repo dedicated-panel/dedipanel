a543abf6ce9ad5cc55eb71ec1434901490e2f798
NiR- <nir@dedicated-panel.net>
2014-12-26 15:42:57 +0100

    Ajout d'une condition a la migration correspondant a la b4, pour qu'elle ne soit pas exécute en cas de maj b4 -> b5.

--- src/DP/Core/DistributionBundle/Migrations/Version20141128024805.php.php
+++ src/DP/Core/DistributionBundle/Migrations/Version20141128024805.php.php
@@ -14,6 +14,14 @@ class Version20141128024805 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        // Cette migration ne doit pas être exécuté si les tables sont déjà présentes
+        // Nécessaire pour la maj b4 -> b5, étant donné que l'ajout des migrations date de la b5
+        $expected = [ 'fos_user_group', 'fos_user_user', 'fos_user_user_group', 'plugin', 'game', 'game_plugin',
+            'machine', 'gameserver', 'gameserver_plugins', 'steam_server', 'minecraft_server', 'acl_classes', 'acl_security_identities',
+            'acl_object_identities', 'acl_object_identity_ancestors', 'acl_entries' ];
+        $current  = iterator_to_array(new \RecursiveIteratorIterator(new \RecursiveArrayIterator($this->connection->query('SHOW TABLES')->fetchAll())), false);
+        $this->skipIf(count(array_diff($expected, $current)) === 0);
         
         $this->addSql('CREATE TABLE fos_user_group (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', UNIQUE INDEX UNIQ_583D1F3E5E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE fos_user_user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, username_canonical VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, email_canonical VARCHAR(255) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, locked TINYINT(1) NOT NULL, expired TINYINT(1) NOT NULL, expires_at DATETIME DEFAULT NULL, confirmation_token VARCHAR(255) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', credentials_expired TINYINT(1) NOT NULL, credentials_expire_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_C560D76192FC23A8 (username_canonical), UNIQUE INDEX UNIQ_C560D761A0D96FBF (email_canonical), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
