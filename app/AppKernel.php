acd5883649ee8cc681b65f909c8472cd87fb3204
NiR- <nir@dedicated-panel.net>
2015-01-13 23:28:51 +0100

    Maj des dependances et ajout du debug bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -48,6 +48,7 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
         }
 
         return $bundles;
